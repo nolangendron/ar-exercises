86141ad782b77b33b9bb204479c9b31a6c940d1f
nolangendron <nolanglengendron@gmail.com>
2019-07-09 20:12:56 +0000

    Adds exercise_4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,13 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Your code goes here ..
+
+surrey = Store.create(name: 'Surrey', annual_revenue: '224000', mens_apparel: 'false', womens_apparel: 'true')
+yaletown = Store.create(name: 'Yaletown', annual_revenue: '430000', mens_apparel: 'true', womens_apparel: 'true')
+whistler = Store.create(name: 'Whistler', annual_revenue: '1900000', mens_apparel: 'true', womens_apparel: 'false')
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store| 
+  puts "Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
